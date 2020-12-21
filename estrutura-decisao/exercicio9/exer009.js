2b7334a3d1ae9f3b1fb5047f7190bfce4eea5db1
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-21 14:53:28 -0300

    resolvendo exercicios do livro

---
+++ estrutura-decisao/exercicio9/exer009.js
@@ -0,0 +1,7 @@
+function orderDecrescente() {
+  let numero1 = Number(document.getElementById("num1").value);
+  let numero2 = Number(document.getElementById("num2").value);
+  let numero3 = Number(document.getElementById("num3").value);
+
+  comparar(numero1, numero2, numero3);
+}
