7f8597e1dc59c4c0f6ebdf75389107c889b0f29b
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-21 19:00:25 -0300

    resolvendo exercicios

--- exer-eloquent-js/exer003.js
+++ exer-eloquent-js/exer003.js
@@ -1,14 +1,15 @@
-var size = 8;
-var board = "";
+let size = 8;
+let boader = "";
 
-for (let y = 0; y < size; y++) {
-  for (let x = 0; x < size; x++) {
+for (let x = 0; x < size; x++) {
+  for (let y = 0; y < size; y++) {
     if ((x + y) % 2 == 0) {
-      board += " ";
+      boader += " ";
     } else {
-      board += "#";
+      boader += "#";
     }
   }
-  board += "\n";
+  boader += "\n";
 }
-console.log(board);
+
+console.log(boader);
