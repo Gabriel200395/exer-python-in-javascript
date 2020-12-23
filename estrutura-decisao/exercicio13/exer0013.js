a3fe685acce39dab5e173b9e0b87ec654c6ee707
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-23 16:26:38 -0300

    exercicio 13 resolvido

--- estrutura-decisao/exercicio13/exer0013.js
+++ estrutura-decisao/exercicio13/exer0013.js
@@ -1,9 +1,29 @@
 function diaDeSemana() {
-  let dia = document.getElementById("semana").value;
+  let dia = Number(document.getElementById("semana").value);
 
   switch (dia) {
     case 1:
-      console.log(`$dia - Domingo`);
+      console.log(`${dia} - Domingo`);
       break;
+    case 2:
+      console.log(`${dia} - Segunda`);
+      break;
+    case 3:
+      console.log(`${dia} - Terça`);
+      break;
+    case 4:
+      console.log(`${dia} - Quarta`);
+      break;
+    case 5:
+      console.log(`${dia} - Quinta`);
+      break;
+    case 6:
+      console.log(`${dia} - Sexta`);
+      break;
+    case 7:
+      console.log(`${dia} - Sabado`);
+      break;
+    default:
+      console.log("valor indefinido");
   }
 }
