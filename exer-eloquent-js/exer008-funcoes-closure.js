9b44c75c911cbce4269188827002d9408e3d202a
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-23 15:40:58 -0300

    entendo melhor sobre closures

--- exer-eloquent-js/exer008-funcoes-closure.js
+++ exer-eloquent-js/exer008-funcoes-closure.js
@@ -4,5 +4,34 @@ function multiplier(factor) {
   };
 }
 
-var twice = multiplier(2);
-console.log(twice(5));
+// var twice = multiplier(2);
+// console.log(twice(5));
+
+// function fontTexto(size) {
+//   return function () {
+//     let texto = (document.getElementById("font").style.fontSize = size + "px");
+//   };
+// }
+
+// function fontTexto1(size) {
+//   return function () {
+//     let texto = (document.getElementById("font").style.fontSize = size + "px");
+//   };
+// }
+
+var font = fontTexto(16);
+var font1 = fontTexto1(40);
+
+function minhaBiblioteca() {
+  return {
+    add5() {
+      return 10 + 5;
+    },
+    add10() {
+      return 10 + 5;
+    },
+  };
+}
+
+console.log(minhaBiblioteca.add5());
+console.log(minhaBiblioteca.add10());
