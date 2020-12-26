61f694e155a18dd0fe16b41da1c41d12a766d345
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-26 17:31:03 -0300

    exercicio resolvido

--- estrutura-decisao/exercicio18/exer0018.js
+++ estrutura-decisao/exercicio18/exer0018.js
@@ -2,19 +2,10 @@ function verificarData() {
   let dia = document.getElementById("dia").value;
   let mes = document.getElementById("mes").value;
   let ano = document.getElementById("ano").value;
-  let resposta = document.getElementById("res");
 
-  if (dia <= 31 || mes > 12 || ano < 0) {
+  if (dia <= 31 || mes <= 12) {
+    console.log("data valida");
+  } else if (dia > 31 || mes > 12 || ano < 0) {
     console.log("data invalida");
-  }
-  if (
-    mes == 1 ||
-    mes == 3 ||
-    mes == 5 ||
-    mes == 7 ||
-    mes == 10 ||
-    (mes == 12 && dia >= 31)
-  ) {
-    console.log("mes com quantidade de dias errado");
   }
 }
