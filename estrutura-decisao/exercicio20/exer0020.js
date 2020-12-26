1d245fac25e416c94d410bbe6fe28b260a30dbc7
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-26 17:48:14 -0300

    exercicio 2o resolvido

---
+++ estrutura-decisao/exercicio20/exer0020.js
@@ -0,0 +1,14 @@
+function validarNota() {
+  let portugues = document.getElementById("portugues").value;
+  let matematica = document.getElementById("matematica").value;
+  let ingles = document.getElementById("ingles").value;
+  let media = (Number(portugues) + Number(matematica) + Number(ingles)) / 3;
+  console.log(media);
+  if (media > 7 && media <= 10) {
+    console.log("aprovado");
+  } else if (media <= 7) {
+    console.log("reprovado");
+  } else if (media >= 10) {
+    console.log("Aprovado com Distinção");
+  }
+}
