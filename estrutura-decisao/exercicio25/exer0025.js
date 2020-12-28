4763a7ce943a14d85079becc8b9779562efccd9c
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-28 11:29:43 -0300

    exercicio 25 resolvido

--- estrutura-decisao/exercicio25/exer0025.js
+++ estrutura-decisao/exercicio25/exer0025.js
@@ -1,7 +1,15 @@
-let numero = Number(prompt("digite um numero"));
+let perguntar = prompt("Telefonou para a vítima?");
+let pergunta1 = prompt("Esteve no local do crime?");
+let pergunta2 = prompt("Mora perto da vítima?");
+let pergunta3 = prompt("Devia para a vítima?");
+let pergunta4 = prompt("Já trabalhou com a vítima?");
 
-if (numero > 0) {
-  console.log("numero inteiro");
+if (perguntar === "sim" && pergunta1 === "sim") {
+  console.log("suspeita!");
+} else if (pergunta2 === "sim" && pergunta3 === "sim") {
+  console.log("cumplice!");
+} else if (pergunta4 === "sim") {
+  console.log("assasino!");
 } else {
-  console.log("numero decimal");
+  console.log("inocente!");
 }
