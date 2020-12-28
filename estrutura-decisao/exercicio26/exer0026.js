1bb86cac9d0c5fbab610e1e6c595cb215bd880fb
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-28 13:05:52 -0300

    exercicio 26 resolvido

--- estrutura-decisao/exercicio26/exer0026.js
+++ estrutura-decisao/exercicio26/exer0026.js
@@ -1,7 +1,36 @@
-let numero = Number(prompt("digite um numero"));
+let cumbustivel = prompt("escreva o tipo do combustivel");
+let litro = Number(prompt("quantidade de litros"));
 
-if (numero > 0) {
-  console.log("numero inteiro");
-} else {
-  console.log("numero decimal");
+if (cumbustivel === "A" && litro <= 20) {
+  let alcool = 1.9;
+  let custoAlcool = litro * alcool;
+  let desconto = (custoAlcool * 3) / 100;
+  let total = custoAlcool - desconto;
+  console.log(
+    "valor total para pagar com desconto " + desconto + " R$ " + total
+  );
+} else if (cumbustivel === "A" && litro > 20) {
+  let alcool = 1.9;
+  let custoAlcool = litro * alcool;
+  let desconto = (custoAlcool * 5) / 100;
+  let total = custoAlcool - desconto;
+  console.log(
+    "valor total para pagar com desconto " + desconto + " R$ " + total
+  );
+} else if (cumbustivel === "G" && litro <= 20) {
+  let gasolina = 2.5;
+  let custoGasolina = litro * gasolina;
+  let desconto = (custoGasolina * 4) / 100;
+  let total = custoGasolina - desconto;
+  console.log(
+    "valor total para pagar com desconto " + desconto + " R$ " + total
+  );
+} else if (cumbustivel === "G" && litro > 20) {
+  let gasolina = 2.5;
+  let custoGasolina = litro * gasolina;
+  let desconto = (custoGasolina * 6) / 100;
+  let total = custoGasolina - desconto;
+  console.log(
+    "valor total para pagar com desconto " + desconto + " R$ " + total
+  );
 }
