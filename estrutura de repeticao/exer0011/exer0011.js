c28505b2731522852f5308f5d3eea48085624ed9
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-12-30 10:50:13 -0300

    exercicio 11 resolvido

---
+++ estrutura de repeticao/exer0011/exer0011.js
@@ -0,0 +1,6 @@
+let inicio = 1;
+let fim = 10;
+
+for (i = inicio; i <= fim; i++) {
+  console.log(i + i);
+}
---
+++ estrutura de repeticao/exer0017/exer0010.js
@@ -0,0 +1,6 @@
+let inicio = 1;
+let fim = 5;
+
+for (i = inicio; i <= fim; i++) {
+  console.log(i);
+}
