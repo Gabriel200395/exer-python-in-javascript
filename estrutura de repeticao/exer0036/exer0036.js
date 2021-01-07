e9154e00092e67ea5c3ea94080e28e1004a4a61a
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2021-01-07 19:53:18 -0300

    exercicio 36  resolvido

--- estrutura de repeticao/exer0036/exer0036.js
+++ estrutura de repeticao/exer0036/exer0036.js
@@ -2,6 +2,12 @@ let tabuada = Number(prompt("valor da tabuada"));
 let inicio = Number(prompt("inicio da tabuada"));
 let fim = Number(prompt("fim de tabuada"));
 
-for (i = inicio; i <= fim; i++) {
-  console.log(`${tabuada} x ${i} = ${tabuada * i}`);
+if (inicio < fim) {
+  for (i = inicio; i <= fim; i++) {
+    console.log(`${tabuada} x ${i} = ${tabuada * i}`);
+  }
+} else {
+  for (i = inicio; i >= fim; i--) {
+    console.log(`${tabuada} x ${i} = ${tabuada * i}`);
+  }
 }
