9b6bea051817d4f05335625237eb5afe4272e22a
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2021-01-09 19:19:38 -0300

    exercicio 46 resolvido

--- estrutura de repeticao/exer0046/exer0046.js
+++ estrutura de repeticao/exer0046/exer0046.js
@@ -1,12 +1,60 @@
-let gabarito = [
-  { 1: "A" },
-  { 2: "B" },
-  { 3: "C" },
-  { 4: "A" },
-  { 5: "E" },
-  { 6: "E" },
-  { 7: "D" },
-  { 8: "C" },
-  { 9: "B" },
-  { 10: "A" },
-];
+let saltosAtleta = [];
+
+function percorrerSaltos(nome, salto, numeroPuloSalto) {
+  if (nome.length == 0) {
+    console.log("erro!!");
+  } else {
+    saltosAtleta.push({ nome, salto, numeroPuloSalto });
+    indiceSaltos(saltosAtleta, maiorMenor);
+  }
+}
+
+function indiceSaltos(pulosAtleta) {
+  return maiorMenor(pulosAtleta);
+}
+
+function maiorMenor(pulosAtleta) {
+  let menorSalto = pulosAtleta[0];
+  let maiorSalto = pulosAtleta[0];
+  for (i = 0; i < pulosAtleta.length; i++) {
+    if (pulosAtleta[i].salto < menorSalto.salto) {
+      menorSalto = pulosAtleta[i];
+    } else if (pulosAtleta[i].salto > maiorSalto.salto) {
+      maiorSalto = pulosAtleta[i];
+    }
+  }
+
+  return calcularMediaSaltos(maiorSalto, menorSalto, pulosAtleta);
+}
+
+function calcularMediaSaltos(maior, menor, pulosAtleta) {
+  let somar = maior.salto + menor.salto;
+  let total = 0;
+
+  if (somar > total) {
+    total = somar;
+  }
+  let media = total / 2;
+
+  console.log(pulosAtleta);
+  console.log(
+    " maior salto: " +
+      maior.salto +
+      " m " +
+      " menor salto: " +
+      menor.salto +
+      " m " +
+      " de salto media: " +
+      media +
+      " m "
+  );
+
+  return maior, menor, pulosAtleta;
+}
+
+function verificarSaltos() {
+  let nome = document.getElementById("nome").value;
+  let salto = Number(document.getElementById("salto").value);
+  let numero = Number(document.getElementById("numero").value);
+  percorrerSaltos(nome, salto, numero);
+}
