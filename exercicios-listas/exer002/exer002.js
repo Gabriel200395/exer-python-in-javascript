bf1270215976274df8696186af4a9efd523d4ba6
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2021-01-12 11:38:25 -0300

    exercicio 01 e 02 e 03 resolvido

---
+++ exercicios-listas/exer002/exer002.js
@@ -0,0 +1,4 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+array.sort((a, b) => (a < b ? 1 : -1));
+console.log(array);
---
+++ exercicios-listas/exer003/exer003.js
@@ -0,0 +1,8 @@
+let array = [1, 2, 3, 4];
+
+let total = 0;
+for (i = 0; i < array.length; i++) {
+  total += array[i] / array.length;
+}
+
+console.log(total);
